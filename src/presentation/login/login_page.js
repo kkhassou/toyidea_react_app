799889a775cffaffb0baa139976c2075af550617
kkhassou <kakegawakouiti@gmail.com>
2023-04-30 11:14:00 +0900

    初回コミット

---
+++ src/presentation/login/login_page.js
@@ -0,0 +1,116 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
+// class LoginPage extends StatefulWidget {
+//   @override
+//   _LoginPageState createState() => _LoginPageState();
+// }
+
+// class _LoginPageState extends State<LoginPage> {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+//   TextEditingController _emailController = TextEditingController();
+//   TextEditingController _passwordController = TextEditingController();
+
+//   // これ、ログイン状態だと、ホーム画面に戻す
+//   checkAuthentication() async {
+//     _auth.authStateChanges().listen((user) {
+//       if (user != null) {
+//         Navigator.pop(context);
+//       }
+//     });
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     this.checkAuthentication();
+//   }
+
+//   login() async {
+//     // if (_formKey.currentState.validate()) {
+//     //   _formKey.currentState.save();
+//     try {
+//       UserCredential user = await _auth.signInWithEmailAndPassword(
+//           email: _emailController.text, password: _passwordController.text);
+//     } catch (e) {
+//       showError(e.toString());
+//     }
+//     // }
+//   }
+
+//   showError(String errorMessage) {
+//     showDialog(
+//         context: context,
+//         builder: (BuildContext context) {
+//           return AlertDialog(
+//             title: Text("Error"),
+//             content: Text(errorMessage),
+//             actions: <Widget>[
+//               ElevatedButton(
+//                 child: Text("OK"),
+//                 onPressed: () {
+//                   Navigator.of(context).pop();
+//                 },
+//               )
+//             ],
+//           );
+//         });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.deepOrangeAccent,
+//         title: Text(
+//           "ログイン",
+//           style: TextStyle(color: Colors.white, fontSize: 25),
+//         ),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Container(
+//           child: Column(
+//             children: <Widget>[
+//               SizedBox(height: 40.0),
+//               Container(
+//                 padding: EdgeInsets.all(32),
+//                 child: Form(
+//                   key: _formKey,
+//                   child: Column(
+//                     children: <Widget>[
+//                       TextFormField(
+//                         controller: _emailController,
+//                         keyboardType: TextInputType.emailAddress,
+//                         decoration: InputDecoration(labelText: "Email"),
+//                       ),
+//                       TextFormField(
+//                         controller: _passwordController,
+//                         obscureText: true,
+//                         decoration: InputDecoration(labelText: "Password"),
+//                       ),
+//                       SizedBox(height: 20.0),
+//                       ElevatedButton(
+//                         style: ButtonStyle(
+//                           minimumSize:
+//                               MaterialStateProperty.all<Size>(Size(150, 40)),
+//                           backgroundColor: MaterialStateProperty.all<Color>(
+//                               Color.fromARGB(200, 50, 205, 50)),
+//                         ),
+//                         onPressed: login,
+//                         child: Text(
+//                           "Login",
+//                           style: TextStyle(color: Colors.white, fontSize: 20.0),
+//                         ),
+//                       )
+//                     ],
+//                   ),
+//                 ),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
